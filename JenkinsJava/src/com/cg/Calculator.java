0ebb1d87654062ec2c8b68448d2226e946e5ed84
Rincy-Cheriyan <Rincy3230@gmail.com>
2020-06-14 19:34:48 +0530

    commit from bash

---
+++ JenkinsJava/src/com/cg/Calculator.java
@@ -0,0 +1,16 @@
+package com.cg;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Calculator cal=new Calculator();
+		cal.add(10,20);
+
+	}
+
+	private void add(int i, int j) {
+		System.out.println(i+j);
+		
+	}
+
+}
